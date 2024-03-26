c848878c6fc77e2cae380383a4b2aa04f60d6c68
billdev1958 <billdev1958@gmail.com>
2024-03-26 15:44:48 -0600

    e

--- src/services/authentication.tsx
+++ src/services/authentication.tsx
@@ -1,11 +1,7 @@
 export const AuthService = {
     login: async (username: string, hash: string): Promise<string | null> => {
       try {
-<<<<<<< HEAD
-        const response = await fetch("https://api.ax01.dev/api/login", {
-=======
         const response = await fetch("https://api.ax01.dev/login", {
->>>>>>> 05eac1a8177809b8637ae3feef7d807c19afcf6a
           method: "POST",
           headers: {
             "Content-Type": "application/json",
